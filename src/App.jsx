21797753741391c34cf5ed62e9664b2366fd687c
Syed Mahad Shah Bukhari <110794379+mahadshahb@users.noreply.github.com>
2022-12-08 21:10:31 +0500

    today-work

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Hero from './components/Hero';
-import { heroapi } from './data/data';
+import { heroapi, popularsales, toprateslaes } from './data/data';
+import { Sales } from './components';
 
 const App = () => {
   return (
     <>
-      <main>
+      <main className='gap-16 relative flex flex-col'>
         <Hero heroapi={heroapi} />
+        <Sales endpoint={popularsales} ifExists/>
+        <Sales endpoint={toprateslaes} />
       </main>
     </>
   )
---
+++ src/components/Sales.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Title from './utils/title';
+import Items from './utils/items';
+
+const Sales = ({ ifExists, endpoint }) => {
+    const { title, items } = endpoint
+    return (
+        <div className='nike-container'>
+            <Title title={title} />
+            <div className={`grid items-center justify-items-center gap-7 lg:gap-5 mt-7 
+            ${ifExists ? 'grid-cols-3 xl:grid-cols-2 sm:grid-cols-1' :
+                    'grid-cols-4 xl:grid-cols-3 md:grid-cols-2 sm:grid-cols-1'}`}>
+                {
+                    items?.map((item, i) => {
+                        return (
+                            <Items {...item} key={i} />
+                        )
+                    })
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Sales;
